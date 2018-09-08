c8ddfc7cfb91e6bd1d92c7ce2764aba7c969f5d9
lstravers <stravers222@gmail.com>
2018-09-08 19:24:42 -0400

    add comments and twerps controllers

--- pajaro_api/app/controllers/application_controller.rb
+++ pajaro_api/app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::API 
+
+  include ActionController::HttpAuthentication::Token::ControllerMethods
+  before_action :authenticate_token
+
+  def authenticate_token
+  
+      user = authenticate_with_http_token do |api_token, options|
+          User.find_by_api_token(token)
+          
+        end
+    
+        unless user
+          render json: { error: "Access Denied" }, status: :unauthorized
+        end
+  end
+
 end
