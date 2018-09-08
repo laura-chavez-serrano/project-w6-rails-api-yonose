cdc59f0b66acc0d22fb8a9ea7cf8b1662a4637ce
lstravers <stravers222@gmail.com>
2018-09-08 17:47:03 -0400

    add serializer gem and user_serializer model

---
+++ pajaro_api/app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,24 @@
+class Api::V1::UsersController < ApplicationController
+  skip_before_action :verify_authentication
+
+  def index
+    @users = User.all
+    render json: @users
+  end
+  
+  
+  def create
+    user = User.new(users_params)
+    if user.save
+      render json: user, status: :ok # or 200
+    else
+      render json: user.errors, status: :bad_request # or 400
+    end
+  end
+  
+  private
+    def users_params
+      params.permit(:username, :password_digest)
+    end
+
+end
