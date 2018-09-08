e5971459a3e6c2702655c9b359cdc3b0ddb6bf9e
lstravers <stravers222@gmail.com>
2018-09-08 14:39:41 -0400

    add twerp controller

---
+++ pajaro_api/app/controllers/twerps_controller.rb
@@ -0,0 +1,55 @@
+class TwerpsController < ApplicationController
+
+
+  before_action :set_user,  only: [:index, :create, :show, :update, :destroy]
+    before_action :set_twerp, only: [:show, :update, :destroy]
+
+  
+    def index
+      @twerps = @user.twerps
+      render json: @user.twerps
+    end
+  
+   
+    def show
+      render json: @twerp
+    end
+
+    
+    def create
+      @twerp = Twerp.create(
+          twerp: params["twerp"],
+          numfavs: params["numfavs"],
+          user_id: @user.id
+          )
+        render json: @twerp
+    end
+  
+    def update
+      if @twerp.update(twerp_params)
+        render json: @twerp
+      else
+        render json: @twerp.errors, status: :unprocessable_entity
+      end
+    end
+    
+    def destroy
+      @twerp.destroy
+    end
+  
+    private
+      # Use callbacks to share common setup or constraints between actions.
+      def set_user
+        @user = User.find(params["user_id"])
+      end
+
+      def set_twerp
+        @twerp= Twerp.find(params["id"])
+      end
+  
+      # Only allow a trusted parameter "white list" through.
+      def twerp_params
+        params.require(:twerp).permit(:twerp, :numfavs)
+      end
+
+end
